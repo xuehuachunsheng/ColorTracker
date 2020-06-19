c7ea71569d4730cfa74ab2ed531fe3ff0c015161
xuehuachunsheng <yeah_imwyx@163.com>
2020-06-19 11:51:54 +0800

    fix bug in color_tracker.cpp line168

---
+++ src/link_demo_video.sh
@@ -0,0 +1,13 @@
+g++.exe `
+-g `
+-o d:\vscode_projects\ColorTracker-master\src\demo_video.exe `
+d:\vscode_projects\ColorTracker-master\src\demo-video.o `
+d:\vscode_projects\ColorTracker-master\src\color_tracker_parameters.o `
+d:\vscode_projects\ColorTracker-master\src\color_tracker.o `
+-I D:\software\opencv3.4.9\build\include `
+-I D:\software\opencv3.4.9\build\include\opencv2 `
+-I D:\software\opencv3.4.9\build\include\opencv `
+-I D:\software\opencv_contrib-3.4.9\modules\tracking\include `
+-I D:\vscode_projects\ColorTracker-master\include `
+-L D:\software\opencv3.4.9\build\x64\MinGW\lib `
+-l opencv_world349
